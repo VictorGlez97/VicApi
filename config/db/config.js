12ad084b104abe793bf6a8dd35cb39af790b8bec
VictorGlez97 <vichgleza@gmail.com>
2022-09-20 22:17:42 -0500

    Add db connection

--- config/db/config.js
+++ config/db/config.js
@@ -1,18 +1,16 @@
 const mysql = require('mysql');
 
 const dbConn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'victors'
+    host: 'us-east.connect.psdb.cloud',
+    user: '29fjzwfu3626rl395w3t',
+    password: 'pscale_pw_nWIy1RzNeJ5ng7c5npdu6h4XYBRHu4BLuIxMnacsRKN',
+    database: 'victors',
+    ssl: true
 });
 
 dbConn.connect(function (err) {
-    
     if (err) throw err;
-
     console.log('Database Connected!!!');
-
 });
 
 module.exports = dbConn;
