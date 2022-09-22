d06d8e51ffbba20dabcb3428a85c7ce34b73c05f
VictorGlez97 <vichgleza@gmail.com>
2022-09-21 21:19:50 -0500

    port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+console.log(`listen on port ${port}`);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
